739c7e8590e96781cb188609daf8e1418e9544b8
mhaikalazizan <mhaikalazizan@gmail.com>
2022-01-24 16:25:32 +0800

    Added new file.

---
+++ examples/add-new-ip-address.php
@@ -0,0 +1,28 @@
+<?php
+
+// include Router Management System class
+require_once "../routermgmt.php";
+
+// set true if you want to allow routerAPI debugging
+$router = new RouterMgmt(false);
+
+// If connection possible, perform next action.
+if($router->connect()){
+
+    $ip         = "192.168.100.1/24";
+    $subnet     = "192.168.100.0";
+    $interface  = "ether10";
+
+    // Get DHCP Client
+    $data = $router->add_ip(
+        $ip,
+        $subnet,
+        $interface
+    );
+
+    // Show data received from router
+    $router->show_debug($data);
+
+    // Disconnect Router
+    $router->disconnect();
+}
