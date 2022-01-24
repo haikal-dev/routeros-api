3977b9b66c187770b631fa5f4ecb46c5135d4ddb
mhaikalazizan <mhaikalazizan@gmail.com>
2022-01-24 15:55:54 +0800

    Updated include file.

--- routermgmt.php
+++ routermgmt.php
@@ -18,14 +18,14 @@ class RouterMgmt
     protected $api;
 
     public function __construct($debug = false){
-        require_once "../../../routeros_api.class.php";
+        require_once "routeros_api.class.php";
 
         $this->api = new RouterosAPI;
         $this->api->debug = $debug;
     }
 
     public function connect(){
-        require_once "../../../credentials.php";
+        require_once "credentials.php";
         $cred = new Credential;
         return $this->api->connect($cred->ip, $cred->port, $cred->username, $cred->password);
     }
