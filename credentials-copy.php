f8b0437e207931f003c9acda9c19f2f0bfff2b49
mhaikalazizan <mhaikalazizan@gmail.com>
2022-01-24 12:06:29 +0800

    Added credentials

---
+++ credentials-copy.php
@@ -0,0 +1,9 @@
+<?php
+
+class Credential
+{
+    public $ip = "192.168.0.1";
+    public $port = "8728";
+    public $username = "<your mikrotik router username>";
+    public $password = "<your mikrotik router password>";
+}
